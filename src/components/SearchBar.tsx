7d7343a473e976d894b4ba7a84242223f076de79
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-02-19 22:01:15 +0630

    Select dom element by using ref in search component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
-const SearchBar = ({ handleSearchQuery, query }: { handleSearchQuery: (target: string) => void; query: string }) => {
+const SearchBar = ({
+  handleSearchQuery,
+  query,
+}: {
+  handleSearchQuery: (target: string) => void;
+  query: string;
+}) => {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl?.current.focus();
+        handleSearchQuery("");
+      }
+    }
+    document.addEventListener("keypress", callback);
+
+    return(() => document.removeEventListener('keypress', callback));
+  }, [handleSearchQuery]);
+
   return (
-    <div className='search-bar-container min-w-[400px]'>
-      <input type="text" onChange={(e) => handleSearchQuery(e.target.value)} value={query} placeholder='search by name' className='w-full px-2 py-1 bg-purple-700 border rounded-md placeholder:text-white focus-within:outline-purple-600' />
+    <div className="search-bar-container min-w-[400px]">
+      <input
+        type="text"
+        ref={inputEl}
+        onChange={(e) => handleSearchQuery(e.target.value)}
+        value={query}
+        placeholder="search by name"
+        className="w-full px-2 py-1 bg-purple-700 border rounded-md placeholder:text-white focus-within:outline-purple-600"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
